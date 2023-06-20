2a93ef859a31fbec4e814540ffa9cfe5730c7174
Basit Korai <Basitalikorai7@gmail.com>
2023-06-20 12:48:36 +0500

    style:

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+// Selecting Elements
+const navbar = document.querySelector('#navbar');
+const navToggleBtn = navbar.querySelector('.nav__btn');
+const navlist = navbar.querySelector('.nav-list');
+// console.log(navlist);
+
+navToggleBtn.addEventListener("click", toggleNavbar);
+
+function toggleNavbar() {
+    navlist.classList.toggle('expanded');
+    navToggleBtn.getAttribute('aria-expanded') === 'false' ? navToggleBtn.setAttribute('aria-expanded', 'true') : navToggleBtn.setAttribute('aria-expanded', 'false');
+}
